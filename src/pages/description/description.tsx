79358d94529f47558eae6fb6575ea737fbf7b02c
Adrian Garcia <adrigar90@gmail.com>
2018-04-23 23:20:51 +0100

    Welcome page

--- src/pages/description/description.tsx
+++ src/pages/description/description.tsx
@@ -7,23 +7,44 @@ export const Description = () => (
     <section
         className="mdl-layout__tab-panel is-active description"
         id="#/">
-        <div>
+        <div className="description__challenges">
             <div className="description-section">
                 <h1>Always looking for new challenges</h1>
-                <p> Building and creating new products from scratch is something really exciting you can not avoid once you've tried it </p>
+                <p>Building and creating new products from scratch is something really exciting you can not avoid once you've tried it </p>
             </div>
         </div>
 
-        <div >
+        <div className="description__team">
             <div className="description-section">
-                <h3>Working as a team is the key to success</h3>
+                <h3>Teamwork as a value</h3>
+                <p> Working in a healthy atmosphere promoting the cooperation</p>
+                <div className="mdl-grid description__team__list">
+                    <div className="mdl-cell mdl-cell--4-col">
+                        <div>
+                            <img src="./../../images/smile.png" alt="" />
+                            <p>Friendly</p>
+                        </div>
+                    </div>
+                    <div className="mdl-cell mdl-cell--4-col">
+                        <div>
+                            <img src="./../../images/thumbs.png" alt="" />
+                            <p>Optimistic</p>
+                        </div>
+                    </div>
+                    <div className="mdl-cell mdl-cell--4-col">
+                        <div>
+                            <img src="./../../images/proactive.png" alt="" />
+                            <p>Proactive</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
 
-        <div className="learning">
+        <div className="description__studies">
             <div className="description-section">
                 <h1>Keep learning</h1>
-                <p> Building and creating new products from scratch is something really exciting you can not avoid once you've tried it </p>
+                <p>Trying to find any opportunity to learn something new and innovative</p>
             </div>
         </div>
 
@@ -32,6 +53,6 @@ export const Description = () => (
                 <h3>Working as a team is the key to success</h3>
             </div>
         </div>
-        
+
     </section>
 );
