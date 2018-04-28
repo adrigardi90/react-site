6222afe7a42747759c8c9eb9644e294789a35f55
Adrian Garcia <adrigar90@gmail.com>
2018-04-28 11:57:57 +0100

    Webpack config

---
+++ config/webpack.common.js
@@ -0,0 +1,133 @@
+var path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+
+var basePath = __dirname;
+var root = path.join(basePath, "..");
+
+module.exports = {
+
+  context: path.join(root, "src"),
+
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx']
+  },
+
+  entry: {
+    app: './main.tsx',
+    appStyles: './app.scss',
+    vendor: [
+      'react',
+      'react-dom',
+      'lodash',
+      'react-router-dom'
+    ]
+  },
+
+  output: {
+    path: path.join(root, 'dist'),
+  },
+
+
+  module: {
+    rules: [
+
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        loader: 'awesome-typescript-loader',
+        options: {
+          useBabel: true,
+        },
+      },
+
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        //exclude: path.join(basePath, 'src/components/'),
+        loader: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            'css-loader',
+            'sass-loader',
+          ]
+        }),
+      },
+
+      // CSS MODULES
+
+      // {
+      //   test: /\.scss$/,
+      //   include: path.join(basePath, 'src/components/'),
+      //   exclude: /node_modules/,
+      //   use: ExtractTextPlugin.extract({
+      //   fallback: 'style-loader',
+      //     use: [
+      //       {
+      //         loader: 'css-loader',
+      //         options: {
+      //           modules: true,
+      //           sourceMap: true,
+      //           // importLoaders: 2,
+      //           localIdentName: '[name]__[local]___[hash:base64:5]',
+      //           camelCase: true,
+      //         }
+      //       },
+      //     'sass-loader'
+      //     ]
+      //   })
+      // },
+
+      {
+        test: /\.css$/,
+        include: /node_modules/,
+        loader: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: {
+            loader: 'css-loader',
+          },
+        }),
+      },
+
+      {
+        test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+      },
+      {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url-loader?limit=10000&mimetype=application/octet-stream'
+      },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url-loader?limit=10000&mimetype=image/svg+xml'
+      },
+      {
+        test: /\.(png|jpg)$/,
+        exclude: /node_modules/,
+        loader: 'url-loader?name=./images/[name].[ext]'
+      },
+      {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'file-loader'
+      },
+
+      {
+        test: /\.html$/,
+        use: 'html-loader'
+      }
+
+    ]
+  },
+  plugins: [
+
+    new ExtractTextPlugin({
+      filename: '[chunkhash].[name].css',
+      disable: false,
+      allChunks: true,
+    }),
+
+    new CopyWebpackPlugin([{
+      from: './static/*.*'
+    }]),
+  ]
+}
--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,33 +1,15 @@
 var path = require('path');
 var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var CopyWebpackPlugin = require('copy-webpack-plugin');
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
 
 var basePath = __dirname;
 var root = path.join(basePath, "..");
 
-module.exports = {
-
-  context: path.join(root, "src"),
-
-  resolve: {
-    extensions: ['.js', '.ts', '.tsx']
-  },
-
-  entry: {
-    app: './main.tsx',
-    appStyles: './app.scss',
-    vendor: [
-      'react',
-      'react-dom',
-      'lodash',
-      'react-router-dom'
-    ]
-  },
+module.exports = merge(common, {
 
   output: {
-    path: path.join(root, 'dist'),
     filename: '[hash].[name].js'
   },
 
@@ -42,92 +24,9 @@ module.exports = {
   },
 
   module: {
-    rules: [
-
-      {
-        test: /\.(ts|tsx)$/,
-        exclude: /node_modules/,
-        loader: 'awesome-typescript-loader',
-        options: {
-          useBabel: true,
-        },
-      },
-
-      {
-        test: /\.scss$/,
-        exclude: /node_modules/,
-        //exclude: path.join(basePath, 'src/components/'),
-        loader: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: [
-            'css-loader',
-            'sass-loader',
-          ]
-        }),
-      },
-
-      // {
-      //   test: /\.scss$/,
-      //   include: path.join(basePath, 'src/components/'),
-      //   exclude: /node_modules/,
-      //   use: ExtractTextPlugin.extract({
-      //   fallback: 'style-loader',
-      //     use: [
-      //       {
-      //         loader: 'css-loader',
-      //         options: {
-      //           modules: true,
-      //           sourceMap: true,
-      //           // importLoaders: 2,
-      //           localIdentName: '[name]__[local]___[hash:base64:5]',
-      //           camelCase: true,
-      //         }
-      //       },
-      //     'sass-loader'
-      //     ]
-      //   })
-      // },
-
-      // {
-      //   test: /\.css$/,
-      //   include: /node_modules/,
-      //   loader: ExtractTextPlugin.extract({
-      //     fallback: 'style-loader',
-      //     use: {
-      //       loader: 'css-loader',
-      //     },
-      //   }),
-      // },
-
-      {
-        test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url-loader?limit=10000&mimetype=application/font-woff'
-      },
-      {
-        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url-loader?limit=10000&mimetype=application/octet-stream'
-      },
-      {
-        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url-loader?limit=10000&mimetype=image/svg+xml'
-      },
-      {
-        test: /\.(png|jpg)$/,
-        exclude: /node_modules/,
-        loader: 'url-loader?name=./images/[name].[ext]'
-      },
-      {
-        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'file-loader'
-      },
-
-      {
-        test: /\.html$/,
-        use: 'html-loader'
-      }
-
-    ]
+    rules: []
   },
+
   plugins: [
 
     new HtmlWebpackPlugin({
@@ -135,19 +34,9 @@ module.exports = {
       template: 'index.html', // Name of template in ./src
       hash: true
     }),
-    new ExtractTextPlugin({
-      filename: '[chunkhash].[name].css',
-      disable: false,
-      allChunks: true,
-    }),
-
-    new CopyWebpackPlugin([
-      //{ from: './images/*.*', to: 'images/', flatten: true },
-      {from : './static/*.*' }
-    ]),
 
     new webpack.optimize.CommonsChunkPlugin({
       names: ['vendor', 'manifest'],
     }),
   ]
-}
+})
