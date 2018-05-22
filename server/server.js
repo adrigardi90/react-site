27ad271e65feaa1371e7a95b34e2586978cb1c6e
Adrian Garcia <adrigar90@gmail.com>
2018-05-22 20:21:15 +0100

    ssl

--- server/server.js
+++ server/server.js
@@ -4,11 +4,17 @@ const app = express();
 const server = http.createServer(app);
 const bodyParser = require('body-parser');
 const path = require('path');
+const https = require('https');
+const fs = require('fs');
 
 const port = 3000;
 
-const router = require('./api')
+const router = require('./api');
 
+const options = {
+    cert: fs.readFileSync('./sslcert/fullchain.pem'),
+    key: fs.readFileSync('./sslcert/privkey.pem')
+};
 
 app.use(express.static(path.join(__dirname, '../dist')));
 
@@ -21,7 +27,5 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../dist/index.html'));
 })
 
-
-server.listen(port, () =>
-    console.log(`connected to localhost:${port}`)
-);
+app.listen(8080);
+//https.createServer(options, app).listen(8443);
