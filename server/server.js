b16b124fc50ac0e45a147db1987151fb1b553498
Adrian Garcia <adrigar90@gmail.com>
2018-05-22 22:49:01 +0100

    ssl

--- server/server.js
+++ server/server.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const http = require('http');
 const app = express();
-const server = http.createServer(app);
 const bodyParser = require('body-parser');
 const path = require('path');
 const https = require('https');
 const fs = require('fs');
 
+// Ports
 const httpPort = 8080;
 const httpsPort = 8443;
 
+// Router
 const router = require('./api');
 
+// SSL certificate configuration
 const options = {
     cert: fs.readFileSync(path.join(__dirname, './sslcert/fullchain.pem')),
     key: fs.readFileSync(path.join(__dirname, './sslcert/privkey.pem'))
@@ -26,10 +28,12 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../dist/index.html'));
 })
 
-app.listen(httpsPort, () => {
+//HTTP
+http.createServer(app).listen(httpsPort, () => {
     console.log("Listening http..." + httpPort);
 });
 
+// HTTPS
 https.createServer(options, app).listen(httpsPort, () => {
     console.log("Listening https..." + httpsPort);
 });
